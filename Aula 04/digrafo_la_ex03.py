c84f1d0f8787e4f2f3ca47a822a5f1a8daa98b42
da-ferreira <ferreiradavid361@gmail.com>
2021-09-01 16:02:44 -0300

    adiciona algoritmo dfs iterativo, aula 04

--- Aula 04/digrafo_la_ex03.py
+++ Aula 04/digrafo_la_ex03.py
@@ -19,13 +19,25 @@ class Digrafo:
         for i in range(len(self.lista_adjacente)):
             print(f"{i}: {', '.join(str(x) for x in self.lista_adjacente[i])}")
 
-    """ Implementa o algoritmo DFS, busca em profundidade, de forma iterativa """
     def busca_em_profundidade(self):
-        visitados = set()
-        pilha = []            
+        """ Implementa o algoritmo DFS, busca em profundidade, de forma iterativa """
+
+        visitados = [False] * self.vertices  # Lista de visitados
+        pilha = []
 
         for vertice in range(self.vertices):
-            pass
+            if not visitados[vertice]:
+                pilha.append(vertice)
+                visitados[vertice] = True
+
+                while len(pilha) > 0:
+                    u = pilha.pop()
+                    print(u)
+
+                    for w in self.lista_adjacente[u]:
+                        if not visitados[w]:
+                            pilha.append(w)
+                            visitados[w] = True
     
 
 if __name__ == "__main__":
@@ -35,6 +47,7 @@ if __name__ == "__main__":
     for aresta in arestas:
         digrafo.insere(aresta[0], aresta[1])
 
-    digrafo.mostra()    
+    digrafo.mostra()
+    print("Busca em profundidade:")    
     digrafo.busca_em_profundidade()
        
