31756f4fbb2b1f4eb42c256c1043a31529c5a30a
da-ferreira <ferreiradavid361@gmail.com>
2021-11-23 18:33:35 -0300

    Modifica aula 16

--- Aula 16/dijkstra.py
+++ Aula 16/dijkstra.py
@@ -1,6 +1,29 @@
 
 class MinHeap:
+    """ Implementa uma fila de prioridades MinHeap. """
     pass
+
+    def __init__(self):
+        pass
+
+    def constroi_heap(self):  # O(n)
+        pass
+
+    def subir(self):  # O(log n)
+        pass
+
+    def inserir(self):  # O(log n)
+        pass
+
+    def descer(self):  # O(log n)
+        pass
+
+    def remover(self):  # O(log n)
+        pass
+
+    def extrair_minimo(self):  # O(log n)
+        pass
+
 
 class Digrafo:
     """ Digrafo utilizando lista de adjacencia. """
@@ -17,7 +40,12 @@ class Digrafo:
         for i in range(self.qtd_vertices):
             print(f"{i}: {', '.join(str(x) for x in self.lista_adjacente[i])}")
 
-    def Dijkstra(self):
+    def Dijkstra(self, s):
+        """
+        Resolve o problema do caminho mínimo de fonte única em um grafo não-direcionado com pesos nas arestas.
+        :param s: vértice inicial.
+        :return: Um vetor de distancia (mínima) do vértice inicial s para todos os vértices e o vetor de pai.
+        """
         pass
 
 
